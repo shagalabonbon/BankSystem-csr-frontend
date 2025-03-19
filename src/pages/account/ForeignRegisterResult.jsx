1b58f11afc62e0ef01895e4f34f1bcb7674b9ea4
Shagalabonbon <ivanqsusan456@gmail.com>
2025-03-19 16:03:28 +0800

    update

--- src/pages/account/ForeignRegisterResult.jsx
+++ src/pages/account/ForeignRegisterResult.jsx
@@ -1,39 +1,40 @@
+import React from "react";
 
 const ForeignRegisterResult = () => {
 
     return (
         <>
-            <div class="text-center fs-1 mt-5 mb-3 ">申請成功</div>
+            <div className="text-center fs-1 mt-5 mb-3 ">申請成功</div>
             {/* <!-- 用 th:if 改寫 交易成功/失敗 --> */}
 
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="bg-light p-4 rounded col-md-4 p-5 ">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="bg-light p-4 rounded col-md-4 p-5 ">
 
-                        <table class="table border bg-transparent">
+                        <table className="table border bg-transparent">
                             <tr>
                                 <td>使用者</td>
-                                <td th:text="${newForeignAccount.getUser.getUsername}"></td>
+                                <td>"newForeignAccount.getUser.getUsername"</td>
                             </tr>
                             <tr>
                                 <td>新建帳號</td>
-                                <td th:text="${newForeignAccount.getAccountNumber}"></td>
+                                <td> "newForeignAccount.getAccountNumber"</td>
                             </tr>
                             <tr>
                                 <td>幣別</td>
-                                <td
-                                    th:text="${newForeignAccount.getCurrency.getName} + ' (' + ${newForeignAccount.getCurrency.getCode} + ')'"></td>
+                                <td>
+                                    "$newForeignAccount.getCurrency.getName + ' (' + newForeignAccount.getCurrency.getCode + ')'"</td>
                             </tr>
                             <tr>
                                 <td>創建時間</td>
-                                <td th:text="${newForeignAccount.getCreateTime}"></td>
+                                <td> "newForeignAccount.getCreateTime"</td>
                             </tr>
 
                         </table>
 
-                        <div class="d-flex justify-content-center">
-                            <form th:action="@{/bank/account/foreign-account}" method="get">
-                                <button type="submit" class="btn btn-lg mt-5 btn-success" >返回首頁</button>
+                        <div className="d-flex justify-content-center">
+                            <form action="@{/bank/account/foreign-account}" method="get">
+                                <button type="submit" className="btn btn-lg mt-5 btn-success" >返回首頁</button>
                             </form>
                         </div>
 
